8512df4b020f8925e7d069191168ac5b1ba54e01
Caio Marques <caiombr@gmail.com>
2024-04-01 23:23:56 -0300

    correção de termos da home escritos

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -21,7 +21,7 @@ function Home() {
         cor={"black"}
         ordenamento={2}
         refer={"/Figurinos"}
-        texto={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"}
+        texto={"Fazemos figurinos para peças, apresentações, exposições, ações e eventos. Venha conferir!"}
         head={"Figurinos"}
         imagem={"https://i.pinimg.com/564x/3f/2b/c7/3f2bc79f33da65a5f4a6738a8a4a8f32.jpg"}
         bg_c={"degrade2"}
@@ -31,7 +31,7 @@ function Home() {
         cor={"black"}
         ordenamento={1}
         refer={"/Festas"}
-        texto={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"}
+        texto={"Confecção de vestidos para Noivas e festas no geral sob medida!"}
         head={"Noivas & Festas"}
         imagem={"https://i.pinimg.com/564x/3f/2b/c7/3f2bc79f33da65a5f4a6738a8a4a8f32.jpg"}
         bg_c={"degrade3"}
@@ -41,7 +41,7 @@ function Home() {
         cor={"whiteR"}
         ordenamento={2}
         refer={"/Autorais"}
-        texto={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"}
+        texto={"Vestimentas feitas com base em modelagens únicas e exclusivas!"}
         head={"Autorais"}
         imagem={"https://i.pinimg.com/564x/3f/2b/c7/3f2bc79f33da65a5f4a6738a8a4a8f32.jpg"}
         bg_c={"degrade4"}
