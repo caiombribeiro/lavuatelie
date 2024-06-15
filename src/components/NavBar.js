06cd618a8f75c49779508bf215e3c80438d829bd
Caio Marques <caiombr@gmail.com>
2024-06-15 11:14:27 -0300

    correção de cores para p/b

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,43 @@
-
+import React, { useState, useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import "./NavBar.css"
 
 function NavBarra() {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar expand="lg" className="cor_bg_navbar fixed-top transparencia" >
-      <Navbar.Brand className="cor_tx_navbar margin_nav" href="/">Lavu Ateliê</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className='margin_right' />
+    <Navbar expand="lg" className={`fixed-top transparencia ${scroll ? 'scroll-navbar' : 'cor_bg_navbar'}`}>
+      <Navbar.Brand className={`margin_nav ${scroll ? 'scroll-text' : 'cor_tx_navbar'}`} href="/">Lavu Ateliê</Navbar.Brand>
+      <Navbar.Toggle 
+        aria-controls="basic-navbar-nav" 
+        className={`margin_right navbar-toggler ${scroll ? 'scroll-toggler' : ''}`} 
+        aria-label="Toggle navigation" 
+      >
+        <span className="navbar-toggler-icon"></span>
+      </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav" className="justify-lg-end justify-center-lg ">
         <Nav className="me-auto marginVW margin_right ">
-          <Nav.Link className="cor_tx_navbar margin" href="/">Home</Nav.Link>
-          <Nav.Link className="cor_tx_navbar margin" href="/Sobre">Sobre</Nav.Link>
-          <Nav.Link className="cor_tx_navbar margin" href="/Figurinos">Figurinos</Nav.Link>
-          <Nav.Link className="cor_tx_navbar margin" href="/Festas">Noivas/Festas</Nav.Link>
-          <Nav.Link className="cor_tx_navbar margin" href="/Autorais">Autorais</Nav.Link>
+          <Nav.Link className={`margin ${scroll ? 'scroll-text' : 'cor_tx_navbar'}`} href="/">Home</Nav.Link>
+          <Nav.Link className={`margin ${scroll ? 'scroll-text' : 'cor_tx_navbar'}`} href="/Sobre">Sobre</Nav.Link>
+          <Nav.Link className={`margin ${scroll ? 'scroll-text' : 'cor_tx_navbar'}`} href="/Figurinos">Figurinos</Nav.Link>
+          <Nav.Link className={`margin ${scroll ? 'scroll-text' : 'cor_tx_navbar'}`} href="/Festas">Noivas/Festas</Nav.Link>
+          <Nav.Link className={`margin ${scroll ? 'scroll-text' : 'cor_tx_navbar'}`} href="/Autorais">Autorais</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,8 @@ import NavBarra from "../components/NavBar"
 import Footeer from '../components/footeer';
 import BlocoPr from '../components/BlocoPr';
 
+import fig1 from '../images/figurinos/figurino1.jpg'
+
 
 
 function Home() {
@@ -13,18 +15,18 @@ function Home() {
     <div className="App">
       <NavBarra />
       
-      <section className='faixap degrade1 fleex'>
+      <section className='faixap whitebackground fleexPainel'>
         <h1 className='size_h1'>Lavu Ateliê</h1>
       </section>
 
       <BlocoPr
-        cor={"black"}
+        cor={"whiteR"}
         ordenamento={2}
         refer={"/Figurinos"}
         texto={"Fazemos figurinos para peças, apresentações, exposições, ações e eventos. Venha conferir!"}
         head={"Figurinos"}
-        imagem={"https://i.pinimg.com/564x/3f/2b/c7/3f2bc79f33da65a5f4a6738a8a4a8f32.jpg"}
-        bg_c={"degrade2"}
+        imagem={fig1}
+        bg_c={"blackbackground"}
       />
 
       <BlocoPr
@@ -33,8 +35,8 @@ function Home() {
         refer={"/Festas"}
         texto={"Confecção de vestidos para Noivas e festas no geral sob medida!"}
         head={"Noivas & Festas"}
-        imagem={"https://i.pinimg.com/564x/3f/2b/c7/3f2bc79f33da65a5f4a6738a8a4a8f32.jpg"}
-        bg_c={"degrade3"}
+        imagem={"https://media.vogue.in/wp-content/uploads/2022/08/289442309_369845058574566_8126628707473336304_n.jpg"}
+        bg_c={"whitebackground"}
       />
 
       <BlocoPr
@@ -43,8 +45,8 @@ function Home() {
         refer={"/Autorais"}
         texto={"Vestimentas feitas com base em modelagens únicas e exclusivas!"}
         head={"Autorais"}
-        imagem={"https://i.pinimg.com/564x/3f/2b/c7/3f2bc79f33da65a5f4a6738a8a4a8f32.jpg"}
-        bg_c={"degrade4"}
+        imagem={"https://s2-marieclaire.glbimg.com/PZABvH9WiMk40ctLRXu38RoJozs=/0x0:1200x1600/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_51f0194726ca4cae994c33379977582d/internal_photos/bs/2022/e/8/6gsCGwSDu9CByruRs8RQ/2022-10-31-2022.jpeg"}
+        bg_c={"blackbackground"}
       />
       <Footeer />
     </div>
